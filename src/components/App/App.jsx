1e34447457f0c68025ed5cb5a01ee04b878842d2
Andre Manchanthasouk <dremanchan@gmail.com>
2022-03-07 15:10:57 -0600

    first half of section 4 questions rendered to page.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,7 +19,8 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
-import SectionOneStory from '../Questionnaire/SectionOneStory';
+import SectionOneStory from '../Questionnaire/SectionOneStory/SectionOneStory';
+import SectionFourTraction from '../Questionnaire/SectionFourTraction/SectionFourTraction';
 
 import './App.css';
 
@@ -102,6 +103,14 @@ function App() {
             path="/testing"
           >
             <SectionOneStory/>
+          </Route>
+
+          <Route
+            exact
+            path="/sectionfour">
+
+              <SectionFourTraction/>
+
           </Route>
 
           <Route
