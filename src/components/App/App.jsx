f0561af3a417a14cbd1cc08a132c8ed2dd3cb9c4
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-03-07 14:12:15 -0600

    tiny changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -104,7 +104,7 @@ function App() {
             exact
             path="/story"
           >
-            {/* <SectionOneStory/> */}
+            <SectionOneStory/>
           </Route>
 
           <Route
