4ca6bc854b82d015140436c431e9cf3c697b8508
Andre Manchanthasouk <dremanchan@gmail.com>
2022-03-08 15:45:44 -0600

    made reducer and saga for section4.

---
+++ src/redux/reducers/section4.reducer.js
@@ -0,0 +1,10 @@
+const section4Reducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_ANSWERS':
+            return action.payload;
+        }
+    
+}
+
+
+export default section4Reducer;
---
+++ src/redux/sagas/section4.saga.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+import { put, takeEvery } from 'redux-saga/effects';
+
+function* section4Saga(){
+    
+}
