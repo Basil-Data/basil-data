6a7093bb44a90ecae0fdadecd30eca64ea64cb09
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-03-10 15:20:11 -0600

    in my get/:id also getting answers to put into reducer

--- server/routes/section1.router.js
+++ server/routes/section1.router.js
@@ -33,14 +33,30 @@ router.get('/:id', async (req, res) => {
         WHERE "enterpriseId" = $1;
     `;
 
+    let sqlText2 = `
+        SELECT
+            "enterpriseSize1",
+            "dateFounded1",
+            "missionStatement1",
+            "understandProblem1",
+            "yearsCollectiveExperience1",
+            "percentageBIPOC1",
+            "percentageFemale1",
+            "investorIntroduction1"
+        FROM "answers"
+        WHERE "enterpriseId" = $1
+    `;
+
     let sqlParams = [
         req.user.id
     ];
 
     const competitiveAdvantagesId = await pool.query(sqlText, sqlParams);
+    const answers = await pool.query(sqlText2, sqlParams);
 
     const results = {
         competitiveAdvantagesId: competitiveAdvantagesId.rows[0].array_agg,
+        ...answers.rows[0]
     }
 
     res.send(results);
