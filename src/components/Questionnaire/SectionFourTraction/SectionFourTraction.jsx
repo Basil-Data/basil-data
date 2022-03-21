2a80de1776a428068bfd3932fda41887dffee9df
Andre Manchanthasouk <dremanchan@gmail.com>
2022-03-21 10:29:45 -0500

    fixed a line.

--- src/components/Questionnaire/SectionFourTraction/SectionFourTraction.jsx
+++ src/components/Questionnaire/SectionFourTraction/SectionFourTraction.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 
@@ -11,22 +11,13 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
 
 import "../Questionnaire.css";
 import QuestionnaireNav from "../QuestionnaireNav/QuestionnaireNav";
-import AdminInputBox from '../../AdminInputBox/AdminInputBox';
-
-const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
 
 function SectionFourTraction() {
   const dispatch = useDispatch();
   const history = useHistory();
-
-  const [open, setOpen] = useState(false);
 
   history.scrollRestoration = 'manual';
 
@@ -53,13 +44,11 @@ function SectionFourTraction() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    setOpen(true);
 
     dispatch({
       type: "UPDATE_SECTION4_ENTERPRISE",
       payload: {
-        ...section4Enterprise,
-        id: selectedEnterprise
+        data: section4Enterprise,
       },
     });
   }
@@ -115,24 +104,8 @@ function SectionFourTraction() {
     }
   };
 
-
-  const handleClose = (event, reason) => {
-      if (reason === 'clickaway') {
-        return;
-      }
-      setOpen(false);
-  };
-
-  const handleAdminDispatch = (event) => {
-    dispatch({
-      type: 'SET_SECTION4_ENTERPRISE',
-      payload: {admin4: event}
-    })
-  }
-
-
   return (
-    <Box className="fullPage">
+    <>
       <QuestionnaireNav />
       <Paper className="formPaper" elevation={3}>
       <form className="questionnaireForm">
@@ -142,56 +115,59 @@ function SectionFourTraction() {
           you are able to provide more detail about the numbers behind
           opportunity for growth and recent customer development.
         </p>
-        <Box sx={{marginTop: "50px"}}>
-          <h4>What stage of development is your PRODUCT in?</h4>
-          <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue={0}
-            value={section4Enterprise.developmentStageId}
-            name="radio-buttons-group"
-            column
-            sx={{marginLeft: "175px"}}
-            onChange={(event) =>
-              dispatch({
-                type: "SET_SECTION4_ENTERPRISE",
-                payload: { developmentStageId: event.target.value },
-              })
-            }
-          >
-            {section4.results1?.map((development) => (
-              <FormControlLabel
-                key={development.id}
-                value={development.id}
-                control={<Radio />}
-                label={development.stage}
-              />
-            ))}
-          </RadioGroup>
-        </Box>
-        <Box sx={{marginTop: "50px"}}>
-          <h4>What stage of investment is your ORGANIZATION in?</h4>
+        <h5>What stage of development is your PRODUCT in?</h5>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue={0}
+          value={section4Enterprise.developmentStageId}
+          name="radio-buttons-group"
+          className="centerHelp"
+          row
+          onChange={(event) =>
+            dispatch({
+              type: "SET_SECTION4_ENTERPRISE",
+              payload: { developmentStageId: event.target.value },
+            })
+          }
+        >
+          {section4.results1?.map((development) => (
+            <FormControlLabel
+              key={development.id}
+              value={development.id}
+              control={<Radio />}
+              label={development.stage}
+              className="centerHelp"
+            />
+          ))}
+        </RadioGroup>
+        <h5>What stage of investment is your ORGANIZATION in?</h5>
 
-          <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue=""
-            value={section4Enterprise.investmentStageId}
-            column
-            sx={{marginLeft: "175px"}}
-            name="radio-buttons-group"
-          >
-            {section4.results2?.map((investment) => (
-              <FormControlLabel
-                key={investment.id}
-                value={investment.id}
-                control={<Radio />}
-                label={investment.stage}
-                onChange={(evt) =>
-                  dispatch({
-                    type: "SET_SECTION4_ENTERPRISE",
-                    payload: {
-                      investmentStageId: evt.target.value,
-                    },
-                  })
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue=""
+          value={section4Enterprise.investmentStageId}
+          row
+          name="radio-buttons-group"
+          className="centerHelp"
+        >
+          {section4.results2?.map((investment) => (
+            <FormControlLabel
+              key={investment.id}
+              value={investment.id}
+              control={<Radio />}
+              label={investment.stage}
+              onChange={(evt) =>
+                dispatch({
+                  type: "SET_SECTION4_ENTERPRISE",
+                  payload: {
+                    investmentStageId: evt.target.value,
+                  },
+                })
+              }
+              className="centerHelp"
+            />
+          ))}
+        </RadioGroup>
 
         <h5>How much have you received in funding to date?</h5>
 
@@ -223,62 +199,20 @@ function SectionFourTraction() {
                 key={use.id}
                 disabled={
                   !progressIndicator.includes(use.id) &&
-                  progressIndicator.length > 1
+                  progressIndicator.length > 2
                 }
+                checked={progressIndicator.includes(use.id)}
+                control={<Checkbox value={use.id} onChange={handleProgress} />}
+                label={use.indicator}
               />
             ))}
-          </RadioGroup>
+          </FormControl>
         </Box>
 
-        <Box sx={{marginTop: "50px"}}>
-          <h4>How much have you received in funding to date?</h4>
-
-          <TextField
-            id="outlined-basic"
-            label="Amount Funded $"
-            variant="outlined"
-            type="number"
-            required
-            InputLabelProps={{ shrink: true }}
-            value={section4Enterprise.fundingReceived4}
-            onChange={(evt) =>
-              dispatch({
-                type: "SET_SECTION4_ENTERPRISE",
-                payload: {
-                  fundingReceived4: evt.target.value,
-                },
-              })
-            }
-          />
-        </Box>
-
-        <Box sx={{marginTop: "50px"}}>
-          <h4>
-            What indicators other than sales show your progress since inception?
-          </h4>
-          <Box sx={{marginLeft: "-100px"}}>
-            <FormControl className="questionnaireForm">
-              {section4.results3?.map((use) => (
-                <FormControlLabel
-                  key={use.id}
-                  disabled={
-                    !progressIndicator.includes(use.id) &&
-                    progressIndicator.length > 2
-                  }
-                  checked={progressIndicator.includes(use.id)}
-                  control={<Checkbox value={use.id} onChange={handleProgress} />}
-                  label={use.indicator}
-                />
-              ))}
-            </FormControl>
-          </Box>
-        </Box>
-
-        <Box>
-        <h4>
+        <h5>
           Tell us more about the progress shown from each of these indicators
           and/or any other unique traction you may have gained.
-        </h4>
+        </h5>
 
         <Grid
           container
@@ -295,76 +229,32 @@ function SectionFourTraction() {
           >
             <TextField
               id="outlined-basic"
-              label="Progress Indicator Explanation"
+              label="Long Answer Text"
               variant="outlined"
               multiline
-              InputLabelProps={{shrink: true,}}
               rows={5}
               fullWidth
-              value={section4Enterprise.progressExplanationOne4}
-              onChange={(evt =>
-                dispatch({
-                  type: 'SET_SECTION4_ENTERPRISE',
-                  payload: {progressExplanationOne4: evt.target.value}
-                }))}
-            />
-          </Box>
-          <br />
-        </Grid>
-
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Box
-            sx={{
-              width: 600,
-              maxWidth: "100%",
-            }}
-          >
-            <TextField
-              id="outlined-basic"
-              label="Progress Indicator 2 Explanation"
-              variant="outlined"
-              multiline
-              InputLabelProps={{shrink: true,}}
-              rows={5}
-              fullWidth
-              value={section4Enterprise.progressExplanationTwo4}
-              onChange={(evt =>
-                dispatch({
-                  type: 'SET_SECTION4_ENTERPRISE',
-                  payload: {progressExplanationTwo4: evt.target.value}
-                }))}
             />
           </Box>
         </Grid>
-        </Box>
 
-        <Box>
-          <h4>What is your customer growth rate over the last 6 months?</h4>
+        <h5>What is your customer growth rate over the last 6 months?</h5>
 
-          <h5>Please provide a percentage</h5>
+        <h6>Please provide a percentage</h6>
 
-          <TextField
-            id="outlined-basic"
-            label="Growth Percentage"
-            variant="outlined"
-            value={section4Enterprise.customerGrowth4}
-            InputLabelProps={{shrink: true,}}
-            onChange={(evt) =>
-              dispatch({
-                type: "SET_SECTION4_ENTERPRISE",
-                payload: { customerGrowth4: evt.target.value },
-              })
-            }
-          />
-        </Box>
-
-   
+        <TextField
+          id="outlined-basic"
+          label="Growth Percentage"
+          variant="outlined"
+          value={section4Enterprise.customerGrowth4}
+          InputLabelProps={{shrink: true,}}
+          onChange={(evt) =>
+            dispatch({
+              type: "SET_SECTION4_ENTERPRISE",
+              payload: { customerGrowth4: evt.target.value },
+            })
+          }
+        />
 
         <h5>Are you generating revenue?</h5>
 
@@ -560,7 +450,7 @@ function SectionFourTraction() {
         </button>
 
         <button onClick={handleSubmit} className="btn">
-          Save
+          Submit
         </button>
 
         
@@ -569,19 +459,8 @@ function SectionFourTraction() {
         </button>
         
       </form>
-
-
-        <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-                  Questionnaire saved!
-              </Alert>
-          </Snackbar>
-        <AdminInputBox
-          value={section4Enterprise.admin4}
-          callback={handleAdminDispatch}
-        />
       </Paper>
-    </Box>
+    </>
   );
 }
 
